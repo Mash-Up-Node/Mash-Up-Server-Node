cb04a3a9700c36aee6dfdd58e0a3ff59a0ba7ccc
Mina Lee <chsjen492@gmail.com>
2026-04-20 22:36:51 +0900

    feat: logger 추가

--- src/common/exception/base-exception.filter.ts
+++ src/common/exception/base-exception.filter.ts
@@ -1,15 +1,17 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
 import { Response } from 'express';
 import { BaseException } from './base.exception';
 
 @Catch(BaseException)
 export class BaseExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(BaseExceptionFilter.name);
+
   catch(exception: BaseException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
 
-    // TODO: stackTrace log 추가
+    this.logger.error(exception.message, exception.stack);
     response.status(status).json(exception.getResponse());
   }
 }
