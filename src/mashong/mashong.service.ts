2034fb29ea20e26afafb00013fd1f8766dc18081
Mina Lee <chsjen492@gmail.com>
2026-05-01 22:43:34 +0900

    refactor: repository 분리

--- src/mashong/mashong.service.ts
+++ src/mashong/mashong.service.ts
@@ -1,19 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { DRIZZLE_DB } from '../db/db.constants';
-import { NodePgDatabase } from 'drizzle-orm/node-postgres';
-import * as schema from '../schema';
-import { and, desc, eq } from 'drizzle-orm';
-import { mashongAttendance } from '../schema';
+import { Injectable } from '@nestjs/common';
 import { CheckAttendanceResponseDto } from './dto/check-attendance.response';
+import { MashongRepository } from './mashong.repository';
 
 const ATTENDANCE_INTERVAL_MS = 30 * 60 * 1000;
 
 @Injectable()
 export class MashongService {
-  constructor(
-    @Inject(DRIZZLE_DB)
-    private readonly db: NodePgDatabase<typeof schema>,
-  ) {}
+  constructor(private readonly mashongRepository: MashongRepository) {}
 
   /**
    * 출석체크를 진행하는 함수
@@ -21,7 +14,8 @@ export class MashongService {
    */
   async checkAttendance(memberId: number): Promise<CheckAttendanceResponseDto> {
     // TODO: 4회 출석 완료 시 redis로 DB 조회 없이 스킵하도록 변경
-    const latestAttendance = await this.getLatestAttendance(memberId);
+    const latestAttendance =
+      await this.mashongRepository.getLatestAttendance(memberId);
     const latestSeq = latestAttendance?.seq ?? 0;
 
     if (latestSeq >= 4) {
@@ -46,10 +40,7 @@ export class MashongService {
 
     const nextSeq = latestSeq + 1;
 
-    await this.db.insert(mashongAttendance).values({
-      memberId,
-      seq: nextSeq,
-    });
+    await this.mashongRepository.insertAttendance(memberId, nextSeq);
 
     // TODO: 미션 기록 반영 추가, insert와 트랜잭션 처리하도록 구현
 
@@ -58,27 +49,5 @@ export class MashongService {
       isChecked: true,
       attendanceSeq: nextSeq,
     };
-  }
-
-  /**
-   * 오늘의 최신 출석 기록을 조회하는 함수
-   * @param memberId
-   * @private
-   */
-  private async getLatestAttendance(memberId: number) {
-    const today = new Date().toISOString().split('T')[0];
-
-    return this.db
-      .select()
-      .from(mashongAttendance)
-      .where(
-        and(
-          eq(mashongAttendance.memberId, memberId),
-          eq(mashongAttendance.attendanceDate, today),
-        ),
-      )
-      .orderBy(desc(mashongAttendance.seq))
-      .limit(1)
-      .then((rows) => rows[0] ?? null);
   }
 }
